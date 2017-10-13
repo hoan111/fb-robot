51c4cd57ec248adab878270fab44c65f6b1b3c95
hoan111 <32768392+hoan111@users.noreply.github.com>
2017-10-13 10:04:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
  */
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'tre_trau') {
+      req.query['hub.verify_token'] === 'subscribe') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
